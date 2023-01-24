5c4d84ea6d84a2db9f74f728fdde38e5c9d079d9
Tobi Saputra <tobisaputra11@gmail.com>
2023-01-25 00:18:15 +0700

    Hutao Theme

---
+++ hutao/theme.bash
@@ -0,0 +1,84 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+# Edited by Andrés Ujpán <@andres04u>
+#
+# Hutao Theme for Archcraft BSPWM v2023
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#29263c'
+foreground='#edecee'
+color0='#15141b'
+color1='#ff6767'
+color2='#61ffca'
+color3='#ffca85'
+color4='#82e2ff'
+color5='#f694ff'
+color6='#61ffca'
+color7='#edecee'
+color8='#15141b'
+color9='#ff6767'
+color10='#61ffca'
+color11='#ffca85'
+color12='#82e2ff'
+color13='#f694ff'
+color14='#61ffca'
+color15='#edecee'
+
+accent='#6d6d6d'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrains Mono:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='manhattan.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Hutao-Dark'
+icon_theme='Zafiro-Green'
+cursor_theme='Qogirr-Dark'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x58'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='12'
+picom_shadow_enable='false'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
